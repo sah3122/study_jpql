b608548707776b3576e9d3ab88dea36b60707c8e
dongchul <sah3122@naver.com>
2019-08-20 22:31:18 +0900

    project setting

---
+++ src/main/java/jpql/Member.java
@@ -0,0 +1,17 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+}
